d82fb911624d2036bf62a98d41d79c0b9e54365f
nvrsunk <nvrsdark@gmail.com>
2025-07-04 15:50:38 +0200

    g

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,9 +7,9 @@ function Footer() {
   
   return (
     <>
-    <div className='bg-black/80 text-center text-white/60'>
+    <div className='bg-black/80 text-white/60 pl-6 pt-6'>
 
-      <li className='flex justify-center gap-4 [&>*>*]:h-4 '>
+      <li className='flex gap-4 [&>*>*]:h-4 '>
           <a href="https://facebook.com"><img src="/images/youtube.svg" alt="" /></a>
           <a href="https://facebook.com"><img src="/images/spotify.svg" alt="" /></a>
           <a href="https://facebook.com"><img src="/images/tik-tok.svg" alt=""/></a> 
@@ -17,33 +17,37 @@ function Footer() {
           <a href="https://facebook.com"><img src="/images/x.svg" alt="" /></a>
       </li>
     
-      <section className='flex justify-around my-10 [&>*>*]:text-[12px] '>
+      <section className='flex my-8 gap-7'>
 
-        <div className='flex flex-col gap-3'>
-          <span className='text-sm'> {textos.greeting} </span>
-          <span className='text-sm'>Cookies</span>
-          <span className='text-sm'>Privacy</span>
+        <div className='flex flex-col gap-6 [&>*>*]:text-[10px] [&>*]:gap-2 '>
+
+          <section className='flex flex-col'>
+            <span className='text-sm'> {textos.greeting} </span>
+            <span className='text-sm'>Cookies</span>
+            <span className='text-sm'>Privacy</span>
+          </section>
+
+          <section className='flex flex-col'>
+            <span className='text-sm'>Store</span>
+            <span className='text-sm'>Tours</span>
+            <span className='text-sm'>Privacy</span>
+          </section>
+
         </div>
 
-        <div className='flex flex-col gap-3'>
-          <span className='text-sm'>Store</span>
-          <span className='text-sm'>Tours</span>
-          <span className='text-sm'>Privacy</span>
-        </div>
-
-        <div className='flex flex-col gap-3'>
+        <div className='flex flex-col [&>*]:text-[10px] gap-2'>
           <span className='text-sm'>Privacy</span>
           <span className='text-sm'>Cookies</span>
           <span className='text-sm'>Privacy</span>
         </div>
       </section>
 
-      <section className='flex flex-col gap-2'>
-      <p className='text-[8px] '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto animi qui unde ea placeat, voluptatum amet recusandae voluptate tempore doloribus ipsam optio, minima iste repellat ratione. Culpa, provident tempora? Excepturi!</p>
-      <span className='text-[9px]'>If you have accessibility or payment issues, contact us as soon as possible!</span>
-      <span className='text-[9px]'>© 2025 Taylor Swift, LLC</span>  
+      <section className='flex flex-col gap-1'>
+      <p className='text-[8px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto animi qui unde ea placeat, voluptatum amet recusandae voluptate tempore doloribus ipsam optio, minima iste repellat ratione. Culpa, provident tempora? Excepturi!</p>
+      <span className='text-[8px] mb-4'>If you have accessibility or payment issues, contact us as soon as possible!</span>
+      <span className='text-[8px] mb-6'>© 2025 Taylor Swift, LLC. <br />All Rights Reserved</span>  
       </section>
-    
+
     </div>
 
     </>
